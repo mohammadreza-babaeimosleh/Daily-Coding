a9a5a1c11aeb61fe2558602a5e088c33461c3636
mohammadreza-babaeimosleh <mohammadrezababaeimosleh@gmail.com>
2026-05-13 16:48:39 +0200

    feat: add q11 solution

---
+++ Q11 - H_index/main.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+
+        h = 0
+        for i, cite in enumerate(citations, start=1):
+            if cite >= i:
+                h = i
+            else:
+                break
+
+        return h
